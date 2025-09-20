8ced6e0ca87af5b91225d83382b3421dd6265809
rayray39 <raynerlimfy@gmail.com>
2025-09-20 22:58:32 +0800

    Add top margin to nav button

--- src/app/Components/NavButton.tsx
+++ src/app/Components/NavButton.tsx
@@ -8,6 +8,7 @@ function NavButton({ buttonText, onClickHandler }:{ buttonText: string, onClickH
             px-[40px]
             rounded-lg
             cursor-default
+            mt-[14px]
         "
         id="custom-nav-button"
         onClick={onClickHandler}>{buttonText}</button>
