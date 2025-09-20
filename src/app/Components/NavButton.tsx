2088dbcaa4e4f8b2b8cfcd9bfda33a904bdf6189
rayray39 <raynerlimfy@gmail.com>
2025-09-20 16:37:12 +0800

    Add custom navigation button

---
+++ src/app/Components/NavButton.tsx
@@ -0,0 +1,17 @@
+
+function NavButton({ buttonText, onClickHandler }:{ buttonText: string, onClickHandler:() => void }) {
+    return <>
+        <button className="
+            bg-[#19183B]
+            text-[#E7F2EF]
+            py-[10px]
+            px-[40px]
+            rounded-lg
+            cursor-default
+        "
+        id="custom-nav-button"
+        onClick={onClickHandler}>{buttonText}</button>
+    </>
+}
+
+export default NavButton
