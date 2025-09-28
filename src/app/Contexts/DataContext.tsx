21b23f03dff7f9ddfb22404f096514e702e9a812
rayray39 <raynerlimfy@gmail.com>
2025-09-28 15:00:36 +0800

    Add data context to share data

---
+++ src/app/Contexts/DataContext.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { useContext, useState, createContext } from "react";
+
+interface DataContextType {
+    // define the types for data and its fields
+    data: {
+        summary: string;
+        style: string 
+    } | null;
+    setData: React.Dispatch<React.SetStateAction<{ summary: string; style: string } | null>>;
+}
+
+
+const DataContext = createContext<DataContextType | null>(null);    // create the context
+
+
+export const DataProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    // create a provider that will allow children components to have access to the shared data
+    const [data, setData] = useState<{ summary: string; style: string } | null>(null);
+
+    const contextValue: DataContextType = {
+        data,
+        setData,
+    };
+
+    return (
+        <DataContext.Provider value={contextValue}>
+            {children}
+        </DataContext.Provider>
+    );
+};
+
+// custom hook
+export const useDataContext = () => {
+    const context = useContext(DataContext);
+    if (!context) {
+        throw new Error("useDataContext must be used within a DataProvider");
+    }
+    return context;
+};
