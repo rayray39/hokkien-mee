5559fb3d477e0e517c0c1a7433990afef5534521
rayray39 <raynerlimfy@gmail.com>
2025-10-18 12:35:07 +0800

    Add about dialog component on main page

---
+++ src/app/Components/CustomDialog.tsx
@@ -0,0 +1,34 @@
+import { Ref } from "react";
+import NavButton from "./NavButton";
+
+interface CustomDialogProps {
+    title:string,
+    body:string,
+    isOpen:boolean,
+    closeDialog:() => void
+}
+
+export default function CustomDialog({
+    title,
+    body,
+    isOpen,
+    closeDialog
+}:CustomDialogProps) {
+    if (!isOpen) {
+        return null
+    } else {
+        return <div className="fixed inset-0 flex justify-center items-center bg-black/10 z-50 px-4">
+            <div className="bg-white rounded-2xl shadow-lg flex flex-col items-center text-center
+                            p-4 sm:p-6 md:p-8 space-y-4 sm:space-y-6
+                            w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+                <div className="text-lg sm:text-xl md:text-2xl font-semibold">{title}</div>
+
+                <div>{body}</div>
+
+                <div className="text-center">
+                    <NavButton buttonText="close" onClickHandler={closeDialog} />
+                </div>
+            </div>
+        </div>
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 'use client'
 import { useRouter } from "next/navigation";
 import NavButton from "./Components/NavButton";
+import CustomDialog from "./Components/CustomDialog";
+import { useState } from "react";
 
 export default function Home() {
     const router = useRouter();
+
+    const [openAboutDialog, setOpenAboutDialog] = useState<boolean>(false);
+
+    const aboutDialogBody:string = `
+        Generate a curated LinkedIn post with just a few clicks, powered by meta's llama-3.3-8b model
+    `
+
+    const openAbout = () => {
+        // open the about dialog (modal)
+        console.log('opening about dialog.');
+        setOpenAboutDialog(true);
+    }
+
+    const closeAbout = () => {
+        // close the aboud dialog (modal)
+        console.log('closing about dialog.');
+        setOpenAboutDialog(false);
+    }
 
     const goMainContentPage = () => {
         // go to the main content (summary page)
@@ -14,9 +34,12 @@ export default function Home() {
         <div>
             <div className="text-center text-4xl font-semibold">HokkienMee🍜</div>
 
-            <div className="text-center">
+            <div className="flex justify-center items-center space-x-8">
                 <NavButton buttonText="start" onClickHandler={goMainContentPage} />
+                <NavButton buttonText="about" onClickHandler={openAbout} />
             </div>
+
+            <CustomDialog title="About HokkienMee" body={aboutDialogBody} isOpen={openAboutDialog} closeDialog={closeAbout}/>
         </div>
     );
 }
