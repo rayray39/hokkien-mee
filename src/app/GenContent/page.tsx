37334c6dbcd04213c376e9f8530092dad518a4f4
rayray39 <raynerlimfy@gmail.com>
2025-10-24 10:23:08 +0800

    Add copy button logic and style

--- src/app/GenContent/page.tsx
+++ src/app/GenContent/page.tsx
@@ -6,7 +6,43 @@ export default function GenContent() {
     const searchParams = useSearchParams();
     const text:string = searchParams.get('text') ?? "";     // retrieve the 'text' param (llm generated output)
 
-    const [genContent, setGenContent] = useState(text);
+    const [genContent, setGenContent] = useState<string>(text);
+
+    const [copied, setCopied] = useState<boolean>(false);
+    const copiedButtonStyle = `
+        bg-green-300
+        text-black
+        py-[10px]
+        px-[40px]
+        rounded-lg
+        cursor-default
+        mt-[18px]
+    `;  // button style after copied
+    const originalButtonStyle = `
+        bg-[#19183B]
+        text-[#E7F2EF]
+        py-[10px]
+        px-[40px]
+        rounded-lg
+        cursor-default
+        mt-[18px]
+    `;  // button style before copied
+
+    const copyGenContent = async () => {
+        console.log('copying to clipboard');
+        try {
+            await navigator.clipboard.writeText(genContent);
+            setCopied(true);
+
+            setTimeout(() => {
+                // reset the copied state after 3s
+                setCopied(false)
+            }, 3000);
+            console.log('successfully copied to clipboard');
+        } catch (error) {
+            console.error('Failed to copy text: ', error);
+        }
+    }
 
     return (
         <div className="w-1/2">
@@ -22,15 +58,9 @@ export default function GenContent() {
             ></textarea>
 
             <div className="text-center">
-                <button className="
-                    bg-[#19183B]
-                    text-[#E7F2EF]
-                    py-[10px]
-                    px-[40px]
-                    rounded-lg
-                    cursor-default
-                    mt-[18px]
-                ">copy</button>
+                <button onClick={copyGenContent} className={copied ? copiedButtonStyle : originalButtonStyle}>
+                    {copied ? "copied" : "copy"}
+                </button>
             </div>
         </div>
     )
